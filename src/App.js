ea57147368f72565cde0edcb9ffddbbb86bbe3b5
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-03 20:09:48 +0200

    ADD
    to Lap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
+import React, {useState} from "react";
 
 function App() {
+
+  const [text , setText] = useState("Primery");
+  // const name="fa fa-times";
+  const name="fa-check";
+
   return (
     <div className="App">
      <div className="control has-icons-right">
+        
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
+          onChange={e => setText(e.target.value)}
 
         />
+
+
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
+          <i className={name} />
         </span> 
+
+    <h2>{text}</h2>
       </div>
     </div>
   );
